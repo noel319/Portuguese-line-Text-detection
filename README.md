a58fe0ed49e0c305f5d17e86f3fdc2a38f64ff0f
noel319 <azeisl917@gmail.com>
2024-07-08 16:50:05 +0300

    init project

--- README.md
+++ README.md
@@ -1,18 +1,52 @@
-# Portuguese-line-Text-dection
+# extrator-texto-em-imagem
+Script simples em python para extrair textos de imagens usando Tesseract e OpenCV
 
-Install the dependencies:
- pip install -r requirements.txt
+# O que você irá precisar
+* Pillow (`pip install pillow`)
+* Tesseract (`pip install pytesseract`)
+* ImageMagick (`pip install wand`)
+* Numpy (`pip install numpy`)
+* OpenCV (`pip install opencv-python`)
 
-Run the main script:
+# Reconhecimento em português
+Caso esteja usando linux, basta executar o comando abaixo para que o tesseract reconheça textos em português\
+`sudo apt-get install tesseract-ocr-por`
 
-python main.py images/sample.jpg
+# Possível problema e como resolvê-lo
+Possívelmente quando você tentar usar o wand para pdf, você irá encontrar o seguinte erro:\
+`python wand PolicyError: not authorized`
 
-utils.py:
+Isso ocorre por que o desenvolvedor não deu permissões para wand converter PDF, então basta editar o arquivo\
+`/etc/ImageMagick-6/policy.xml`
 
-Contains the load_and_preprocess_image function that handles loading and preprocessing of the image, including resizing and normalizing.
-main.py:
+E alterar o estado de rights="none" para rights="read" na linha do que se refere a PDF\
+`<policy domain="coder" rights="read" pattern="PDF" />`
 
-Loads the TrOCR processor and model.
-Preprocesses the image using the function from utils.py.
-Recognizes the text from the image using the TrOCR model.
-Prints the recognized text.
+# Como executar
+O script recebe 2 parametros, 1° o caminho da pasta que está os pdfs para extração e o 2° que é o caminho do arquivo que ele irá gerar em txt
+
+`python main.py /home/caminho-pasta-arquivo /home/caminho-arquivo-txt`
+
+# Exemplo
+## Pdf de exemplo
+![alt text](https://i.imgur.com/1F0JcDC.png)
+
+## Output
+```
+A Simple PDF File
+
+This is a small demonstration .pdf file -
+
+just for use in the Virtual Mechanics tutorials. More text. And more
+text. And more text. And more text. And more text.
+
+And more text. And more text. And more text. And more text. And more
+text. And more text. Boring, zzzzz. And more text. And more text. And
+more text. And more text. And more text. And more text. And more text.
+And more text. And more text.
+
+And more text. And more text. And more text. And more text. And more
+text. And more text. And more text. Even more. Continued on page 2
+```
+# Créditos
+Não teria conseguido otimizar a leitura se não fosse esse [post](https://github.com/tesseract-ocr/tesseract/wiki/ImproveQuality) feito pelo próprio Tesseract sobre otimização de imagem
--- main.py
+++ main.py
@@ -1,36 +1,64 @@
-import os
-import sys
-from transformers import TrOCRProcessor, VisionEncoderDecoderModel
+import pandas as pd
 import torch
-from utils import load_and_preprocess_image
+from torch.utils.data import Dataset
+from PIL import Image
 
-def recognize_handwritten_text(image_path):
-    # Load the TrOCR processor and model
-    processor = TrOCRProcessor.from_pretrained('microsoft/trocr-base-handwritten')
-    model = VisionEncoderDecoderModel.from_pretrained('microsoft/trocr-base-handwritten')
-    
-    # Preprocess the image
-    image = load_and_preprocess_image(image_path)
-    
-    # Prepare the image for the model
-    pixel_values = processor(images=image, return_tensors="pt").pixel_values
-    
-    # Generate the text from the image
-    generated_ids = model.generate(pixel_values)
-    generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
-    
-    return generated_text
+df = pd.read_fwf('/content/drive/MyDrive/TrOCR/Tutorial notebooks/IAM/gt_test.txt', header=None)
+df.rename(columns={0: "file_name", 1: "text"}, inplace=True)
+del df[2]
+df.head()
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python main.py path_to_image")
-        sys.exit(1)
-    
-    image_path = sys.argv[1]
-    
-    if not os.path.isfile(image_path):
-        print(f"File not found: {image_path}")
-        sys.exit(1)
-    
-    recognized_text = recognize_handwritten_text(image_path)
-    print("Recognized Text:\n", recognized_text)
+from sklearn.model_selection import train_test_split
+
+train_df, test_df = train_test_split(df, test_size=0.2)
+# we reset the indices to start from zero
+train_df.reset_index(drop=True, inplace=True)
+test_df.reset_index(drop=True, inplace=True)
+
+import torch
+from torch.utils.data import Dataset
+from PIL import Image
+
+class IAMDataset(Dataset):
+    def __init__(self, root_dir, df, processor, max_target_length=128):
+        self.root_dir = root_dir
+        self.df = df
+        self.processor = processor
+        self.max_target_length = max_target_length
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        # get file name + text 
+        file_name = self.df['file_name'][idx]
+        text = self.df['text'][idx]
+        # prepare image (i.e. resize + normalize)
+        image = Image.open(self.root_dir + file_name).convert("RGB")
+        pixel_values = self.processor(image, return_tensors="pt").pixel_values
+        # add labels (input_ids) by encoding the text
+        labels = self.processor.tokenizer(text, 
+                                          padding="max_length", 
+                                          max_length=self.max_target_length).input_ids
+        # important: make sure that PAD tokens are ignored by the loss function
+        labels = [label if label != self.processor.tokenizer.pad_token_id else -100 for label in labels]
+
+        encoding = {"pixel_values": pixel_values.squeeze(), "labels": torch.tensor(labels)}
+        return encoding
+from transformers import TrOCRProcessor
+
+processor = TrOCRProcessor.from_pretrained("microsoft/trocr-base-handwritten")
+train_dataset = IAMDataset(root_dir='/content/drive/MyDrive/TrOCR/Tutorial notebooks/IAM/image/',
+                           df=train_df,
+                           processor=processor)
+eval_dataset = IAMDataset(root_dir='/content/drive/MyDrive/TrOCR/Tutorial notebooks/IAM/image/',
+                           df=test_df,
+                           processor=processor)
+print("Number of training examples:", len(train_dataset))
+print("Number of validation examples:", len(eval_dataset))
+
+encoding = train_dataset[0]
+for k,v in encoding.items():
+  print(k, v.shape)
+image = Image.open(train_dataset.root_dir + train_df['file_name'][0]).convert("RGB")
+   
